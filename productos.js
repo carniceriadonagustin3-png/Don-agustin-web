8f4caf820c9c90906cf46ca6f3053a80b0ee49c3
carniceriadonagustin3-png <carniceriadonagustin3@gmail.com>
2026-09-03 23:27:19 -0300

    Update productos.js

--- productos.js
+++ productos.js
@@ -11,40 +11,62 @@ const products = [
   {
     category: "Vacuno",
     items: [
-      { name: "Asado", price: "" },
-      { name: "Vacío", price: "" },
-      { name: "Matambre", price: "" },
-      { name: "Colita de cuadril", price: "" },
-      { name: "Tapa de asado", price: "" },
-      { name: "Marucha", price: "" },
-      { name: "Costeleta", price: "" },
-      { name: "Pulpas", price: "" }
+      { name: "Picada", price: "$12.000" },
+{ name: "Milanesas de carne", price: "$14.000" },
+{ name: "Hamburguesas (kg)", price: "$9.000" },
+{ name: "Nalga", price: "$20.000" },
+{ name: "Cuadrada", price: "$20.000" },
+{ name: "Cuadril", price: "$20.000" },
+{ name: "Bola de lomo", price: "$20.000" },
+{ name: "Lomo", price: "$21.000" },
+{ name: "Peceto", price: "$21.000" },
+{ name: "Puchero surtido", price: "$9.800" },
+{ name: "Osobuco", price: "$12.500" },
+{ name: "Falda", price: "$17.000" },
+{ name: "Asotillo", price: "$17.000" },
+{ name: "Marucha", price: "$20.000" },
+{ name: "Costilla", price: "$20.000" },
+{ name: "Vacío", price: "$20.000" },
+{ name: "Tapa de asado", price: "$20.000" },
+{ name: "Matambre", price: "$20.000" },
+{ name: "Centro de entraña", price: "$15.000" },
+{ name: "Costeleta", price: "$18.000" },
+{ name: "Aguja especial", price: "$18.000" },
+{ name: "Aguja común", price: "$16.500" }
     ]
   },
   {
     category: "Cerdo",
     items: [
       { name: "Costilla", price: "$10.900" },
-      { name: "Vacío", price: "$10.900" },
-      { name: "Matambre", price: "$14.500" },
-      { name: "Tapa", price: "$10.900" },
-      { name: "Marucha", price: "$9.900" },
-      { name: "Costeleta", price: "$8.500" },
-      { name: "Bondiola", price: "$11.000" },
-      { name: "Pulpas", price: "$10.900" },
-      { name: "Pechito", price: "$10.900" },
-      { name: "Milanesas de cerdo", price: "$8.600" },
-      { name: "Rueda entera", price: "$9.800" },
-      { name: "Solomillo", price: "$9.900" }
+{ name: "Vacío", price: "$10.900" },
+{ name: "Matambre", price: "$14.500" },
+{ name: "Tapa", price: "$10.900" },
+{ name: "Marucha", price: "$10.900" },
+{ name: "Costeleta", price: "$9.900" },
+{ name: "Bondiola", price: "$8.500" },
+{ name: "Pulpas todas", price: "$11.000" },
+{ name: "Pechito", price: "$10.900" },
+{ name: "Milanesa de cerdo", price: "$8.600" },
+{ name: "Rueda entera", price: "$9.800" },
+{ name: "Solomillo", price: "$9.900" },
+{ name: "Cabeza de cerdo y patitas", price: "$5.000" }
     ]
   },
   {
     category: "Pollo y otros",
     items: [
       { name: "Pollo", price: "" },
-      { name: "Pechuga", price: "" },
-      { name: "Hígado", price: "" },
-      { name: "Chorizos", price: "" }
+      { name: "Chorizos", price: "$9.500" },
+{ name: "Hígado", price: "$6.700" },
+{ name: "Milanesas de hígado", price: "$4.900" },
+{ name: "Pollo entero", price: "$4.900" },
+{ name: "Milanesas de pollo", price: "$7.900" },
+{ name: "Pechuga", price: "$10.000" },
+{ name: "Alitas", price: "$4.000" },
+{ name: "Recortes", price: "$1.500" },
+{ name: "Patamuglo", price: "$4.900" },
+{ name: "Filete de pollo", price: "$11.200" }
     ]
   }
 ];
